7108aed8b8b1401f587090a5c5000031d31ce9eb
kumar <kumar@LavanyaKumar>
2024-01-02 15:23:08 +0530

    edit

---
+++ simple/src/test/java/Demopractice/prcatice.java
@@ -0,0 +1,11 @@
+package Demopractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class prcatice {
+	@Test
+	public void prac() {
+		Reporter.log("edit and send back");
+	}
+}
